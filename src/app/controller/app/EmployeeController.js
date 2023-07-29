0ae25f9a9111bf297a90897faf45e99fc76f9023
risalat-shamoa <risalat.shamoa@quokkalabs.com>
2023-07-29 08:08:37 +0530

    add filters in all employees end-point

--- src/app/controller/app/EmployeeController.js
+++ src/app/controller/app/EmployeeController.js
@@ -1,6 +1,9 @@
-const { EmployeeListResponse } = require("../../../resources/EmployeeListResponse");
-const EmployeeService = require("../../../services/EmployeeService");
-const responseHandler = require("../../../utils/responseHandler");
+const {
+  EmployeeListResponse,
+} = require('../../../resources/EmployeeListResponse')
+const EmployeeService = require('../../../services/EmployeeService')
+const CompanyService = require('../../../services/CompanyService')
+const responseHandler = require('../../../utils/responseHandler')
 
 exports.getAllEmployees = async (request, response, next) => {
   try {
@@ -11,20 +14,39 @@ exports.getAllEmployees = async (request, response, next) => {
     if (String(typeof data) === "number") {
       return responseHandler(request, response, next, false, data, {});
     }
-    return responseHandler(request, response, next, true, 3047, EmployeeListResponse.collection(data))
+    return responseHandler(
+      request,
+      response,
+      next,
+      true,
+      3047,
+      EmployeeListResponse.collection(data)
+    )
   } catch (err) {
-    next(err);
+    next(err)
   }
-};
+}
 
 exports.createEmployeeProfile = async (request, response, next) => {
   try {
     const data = await EmployeeService.createEmployeeProfile(
       request.user.id,
       request.body
-    );
-    return responseHandler(request, response, next, true, 3045, data);
+    )
+    return responseHandler(request, response, next, true, 3045, data)
   } catch (err) {
-    next(err);
+    next(err)
   }
-};
+}
+
+exports.contactCompany = async (request, response, next) => {
+  try {
+    const data = await CompanyService.contactCompany(
+      request.user,
+      request.body
+    )
+    return responseHandler(request, response, next, true, 3048, data)
+  } catch (err) {
+    next(err)
+  }
+}
